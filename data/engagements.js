e545c8ea90ee163bc2c3afd090a7bd5401a2210d
Christoffer Lambertsson <github@lambertsson.me>
2020-07-26 18:49:07 +0200

    Added new engagement and tags for existing engagements

--- data/engagements.js
+++ data/engagements.js
@@ -1,17 +1,40 @@
 const engagements = [
   {
-    title: "Loyalty program at bank",
-    description: "A short description if needed",
+    title: "New loyalty program at bank",
+    description: "Fullstack engagement for a bank creating their new loyalty program, giving good vibes only.",
+    tags: ["JS", "Node.js", "React Native", "React.js", "Jest", "Git", "Jira", "AWS", "DynamoDB", "PostgreSQL", "REST"],
     imageUrl: "icons/image-solid.svg",
     highlight: true,
-    year: 2019
+    year: 2020,
+  },
+  {
+    title: "Loyalty program at bank",
+    description:
+      "Fullstack engagement for a bank developing their loyalty program, both surprising and delighting customers.",
+    tags: [
+      "JS",
+      "Node.js",
+      "React Native",
+      "React.js",
+      "Jest",
+      "Git",
+      "Jira",
+      "HTML/CSS",
+      "PostgreSQL",
+      "Redis",
+      "REST",
+    ],
+    imageUrl: "icons/image-solid.svg",
+    highlight: false,
+    year: 2019,
   },
   {
     title: "E-sports streaming service",
     description:
       "Fullstack engagement helping an e-sports streaming start-up level up their gamification and launch their new betting hub.",
+    tags: ["Vue.js", "JS", "HTML/CSS", "Git", "Trello", "AWS", "RabbitMQ", "REST", "PostgreSQL"],
     imageUrl: "icons/image-solid.svg",
     highlight: true,
-    year: 2018
-  }
+    year: 2018,
+  },
 ];
